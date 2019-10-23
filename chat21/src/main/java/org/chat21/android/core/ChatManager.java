06a2aadbcba058f4027f6a2bd9bc0665ea2b8f8c
Martin Kovachev <miracle@nimasystems.com>
2019-10-23 16:05:01 +0300

    Changes

--- chat21/src/main/java/org/chat21/android/core/ChatManager.java
+++ chat21/src/main/java/org/chat21/android/core/ChatManager.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -25,6 +27,7 @@ import org.chat21.android.core.presence.MyPresenceHandler;
 import org.chat21.android.core.presence.PresenceHandler;
 import org.chat21.android.core.users.models.ChatUser;
 import org.chat21.android.core.users.models.IChatUser;
+import org.chat21.android.utils.ChatUtils;
 import org.chat21.android.utils.IOUtils;
 import org.chat21.android.utils.StringUtils;
 
@@ -548,6 +551,44 @@ public class ChatManager {
 //                "fullnameDAELIMINAREANDROID",
 //                Message.TYPE_TEXT, text, customAttributes, sendMessageListener);
 //    }
+
+    public void updateFCMToken() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String appId = ChatManager.Configuration.appId;
+
+        if (firebaseUser != null && StringUtils.isValid(appId)) {
+
+            DatabaseReference root;
+            if (StringUtils.isValid(ChatManager.Configuration.firebaseUrl)) {
+                root = FirebaseDatabase.getInstance()
+                        .getReferenceFromUrl(ChatManager.Configuration.firebaseUrl);
+            } else {
+                root = FirebaseDatabase.getInstance().getReference();
+            }
+
+            DatabaseReference firebaseUsersPath = root
+                    .child("apps/" + ChatManager.Configuration.appId +
+                            "/users/" + firebaseUser.getUid() + "/instances/" + token);
+
+            Map<String, Object> device = new HashMap<>();
+            device.put("device_model", ChatUtils.getDeviceModel());
+            device.put("platform", "Android");
+            device.put("platform_version", ChatUtils.getSystemVersion());
+            device.put("language", ChatUtils.getSystemLanguage(mContext.getResources()));
+
+            firebaseUsersPath.setValue(device); // placeholder value
+
+            Log.i(DEBUG_LOGIN, "SaveFirebaseInstanceIdService.onTokenRefresh: " +
+                    "saved with token: " + token +
+                    ", appId: " + appId + ", firebaseUsersPath: " + firebaseUsersPath);
+        } else {
+            Log.i(DEBUG_LOGIN, "SaveFirebaseInstanceIdService.onTokenRefresh:" +
+                    "user is null. token == " + token + ", appId == " + appId);
+        }
+    }
 
     public void sendTextMessage(String recipientId, String recipientFullName, String text) {
         sendTextMessage(recipientId, recipientFullName, text,
